c046d06710fdf735996b1d9e5eb2723f976ff3bd
Sinyakov Gleb Sergeevich <g.sinyakov@tinkoff.ru>
2019-01-22 20:35:56 +0300

    Add cycle iterator example.
    Add an example of iterator, that infinitely loops over a collection in cycles.

---
+++ cycle_iterator.py
@@ -0,0 +1,19 @@
+class CycleIterator:
+
+    def __init__(self, base_list):
+        self.base_list = base_list
+        self.length = len(base_list)
+        self.counter = -1
+
+    def __next__(self):
+        if self.counter == self.length-1:
+            self.counter = -1
+
+        self.counter += 1
+        return self.base_list[self.counter]
+
+
+iterator = CycleIterator([1, 2, 3])
+
+for i in range(10):
+    print(next(iterator))

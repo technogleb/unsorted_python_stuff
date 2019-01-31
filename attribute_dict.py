6ccb2536a707508549f379e08ea43cc4f76edd14
Sinyakov Gleb Sergeevich <sinyakov@phystech.edu>
2019-01-31 18:32:18 +0300

    A better implementation of attribute_dict

--- attribute_dict.py
+++ attribute_dict.py
@@ -11,5 +11,16 @@ class AttributeDict(dict):
         else:
             return self[item]
 
+
+"""A better implementation, __init__ method is excess,
+   if we don't need to add any custom constructor logic"""
+class A(dict):
+    def __getattr__(self, item):
+        if item not in self.keys():
+            return KeyError
+        else:
+            return self[item]
+
+
 upgraded_dict = AttributeDict({'a': 5, 'b': 7})
 print(upgraded_dict['a'])

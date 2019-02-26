72f86292f705ac60b8595316b90aabd6ec92d8d8
Gleb Sinyakov <sinyakov@phystech.edu>
2019-02-26 18:49:20 +0300

    Add contextlib implementation of simple timer

--- simple_timer.py
+++ simple_timer.py
@@ -1,4 +1,5 @@
 """Just a simple timer using context manager."""
+from contextlib import contextmanager
 
 class SimpleTimer:
     def __enter__(self):
@@ -6,3 +7,12 @@ class SimpleTimer:
     
     def __exit__(self, type, values, traceback):
         print("It took {}".format(time.time() - self.start))
+
+
+# or using contextlib package
+@contextmanager
+def timer():
+    start = time.time()
+    yield start
+    end = time.time()
+    print("It took {}".format(end-start))

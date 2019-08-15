f077d4ff8d958439472c3e7dabecaf8c59f4856a
technogleb <g.sinyakov@phystech.edu>
2019-08-15 17:10:00 +0300

    PEP8 and docstring refactor

--- postgres_context_manager.py
+++ postgres_context_manager.py
@@ -1,17 +1,30 @@
-"""
-Context manager to work with postgresql python adapter (psycopg2).
+"""Context manager to work with postgresql python adapter (psycopg2)."""
 
-Usage example:
---------------
->>> db = DatabaseHandler(user='user', password='password', server='host', database='postgres')
-with db:
-	res = db.execute(sql, params)
-    for row in res:
-		do_something(row)
-"""
+import psycopg2
+from psycopg2 import extras
+import getpass
 
 
 class DatabaseHandler:
+    """
+    Just a simple postgres manager built as context manager for ease of use,
+    that simultaneously opens/closes both connection and cursor.
+    Also uses dict cursor.
+
+    Usage example:
+    --------------
+    >>> db = DatabaseHandler(
+    ...        user='user', password='password', server='host', database='postgres')
+    >>> sql = "select * from table where timestamp < %s and timestamp > %s"
+    >>> params = ('2019-06-03', '2019-06-01')
+    >>> def do_something(row):
+    ...     print(row['field'])
+    >>> with db:
+    ...     res = db.execute(sql, params)
+    ...     for row in res:
+    ...         do_something(row)
+    """
+
     def __init__(self, user='postgres', password='postgres',
                  server='ipv4', database='postgres'):
         self.user = user

e7db735819bd59f340f86f65430dd3ad7c8037bf
technogleb <sinyakov@phystech.edu>
2019-08-15 17:13:56 +0300

    Change connection params setting
    Each parameter now is a property, that has setting logic as follows:
    if there is env variable for that, set it as parameter, else if user
    provided it in creating instance of a class, set this as parameter, else
    ask user to prompt it.

--- postgres_context_manager.py
+++ postgres_context_manager.py
@@ -1,7 +1,7 @@
 """Context manager to work with postgresql python adapter (psycopg2)."""
-
+import os
 import psycopg2
-from psycopg2 import extras
+from psycopg2 import extras, sql
 import getpass
 
 
@@ -24,32 +24,83 @@ class DatabaseHandler:
     ...     for row in res:
     ...         do_something(row)
     """
-
-    def __init__(self, user='postgres', password='postgres',
-                 server='ipv4', database='postgres'):
+    def __init__(self, user='', password='', host='', dbname=''):
+        self.host = host
+        self.dbname = dbname
         self.user = user
-        self._password = password
-        self.server = server
-        self.database = database
+        self.password = password
         self.conn = None
         self.cursor = None
 
-    def execute(self, sql, params=None):
-        self.cursor.execute(sql, params)
+    def execute(self, sql_string, params=None):
+        sql_obj = sql.SQL(sql_string)
+        self.cursor.execute(sql_obj, params)
         return self.cursor
 
     @property
+    def host(self):
+        return self._host
+
+    @property
+    def dbname(self):
+        return self._dbname
+
+    @property
+    def user(self):
+        return self._user
+
+    @property
     def password(self):
-        if self._password == 'postgres':
-            return self._password
-        return getpass.getpass("Enter password for {}: ".format(self.user))
+
+        return self._password
+
+    @host.setter
+    def host(self, signature_host):
+        gp_host = os.getenv('GP_HOST')
+        if gp_host:
+            self._host = gp_host
+        elif signature_host:
+            self._host = signature_host
+        else:
+            self._host = input("Enter greenplum hostname or ip: ")
+
+    @dbname.setter
+    def dbname(self, signature_dbname):
+        gp_dbname = os.getenv('GP_DBNAME')
+        if gp_dbname:
+            self._dbname = gp_dbname
+        elif signature_dbname:
+            self._dbname = signature_dbname
+        else:
+            self._dbname = input("Enter greenplum dbname: ")
+
+    @user.setter
+    def user(self, signature_user):
+        gp_user = os.getenv('GP_USER')
+        if gp_user:
+            self._user = gp_user
+        elif signature_user:
+            self._user = signature_user
+        else:
+            self._user = input("Enter greenplum username: ")
+
+    @password.setter
+    def password(self, signature_password):
+        gp_pass = os.getenv('GP_PASSWORD')
+        if gp_pass:
+            self._password = gp_pass
+        elif signature_password:
+            self._password = signature_password
+        else:
+            self._password = getpass.getpass(
+                "Enter password for {}: ".format(self.user))
 
     def __enter__(self):
         self.conn = psycopg2.connect(
-            host=self.server,
+            host=self.host,
+            dbname=self.dbname,
             user=self.user,
             password=self.password,
-            dbname=self.database
         )
         self.cursor = self.conn.cursor(
             'named cursor', cursor_factory=extras.RealDictCursor)

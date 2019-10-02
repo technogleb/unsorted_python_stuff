ca75b6082351145a3be487fdee06da3a57f77ec1
technogleb <sinyakov@phystech.edu>
2019-10-02 03:40:46 +0300

    Modify timer

--- simple_timer.py
+++ simple_timer.py
@@ -1,10 +1,13 @@
 """Just a simple timer using context manager."""
 from contextlib import contextmanager
+import time
+
 
 class SimpleTimer:
+    """Timer put in a form of context manager."""
     def __enter__(self):
         self.start = time.time()
-    
+
     def __exit__(self, type, values, traceback):
         print("It took {}".format(time.time() - self.start))
 
